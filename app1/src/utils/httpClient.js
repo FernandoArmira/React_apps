995f42eac9218edf6937330e45b22dd71f1f6044
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2022-07-13 14:02:19 -0600

    Correcciones

--- app1/src/utils/httpClient.js
+++ app1/src/utils/httpClient.js
@@ -1,9 +1,10 @@
-const API = "https://api.themoviedb.org/3"
+const API = process.env.REACT_APP_API
+const API_Token = process.env.REACT_APP_API_TOKEN
 
 export function get(path){
     return fetch(API + path ,{
             headers: {
-                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjVmODU0ZTUxN2E2OTEwY2MyNDg1NzNhYTZiMDFjYiIsInN1YiI6IjYyYmY5OTFlMWU2NDg5MDA1ZWZjOTY0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f9f1HxVLH5-mzyHcIroPPB21u4PFV1H3lyQXWPNyaOo",
+                Authorization: "Bearer" + API_Token,
                 "Content-Type": "application/json;charset=utf-8"
             },
         }).then((result) => result.json())
