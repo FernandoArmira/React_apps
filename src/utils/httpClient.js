4f368f193bb7890e5ddc02b279dbbf2ff95fd5e5
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2022-07-15 18:43:07 -0600

    Correccion variable de entorno

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,10 +1,13 @@
 const API = process.env.REACT_APP_API
-const API_Token = process.env.REACT_APP_API_TOKEN
+const API_Token = "Bearer " + process.env.REACT_APP_API_TOKEN
+
+//console.log(API)
+//console.log(API_Token)
 
 export function get(path){
     return fetch(API + path ,{
             headers: {
-                Authorization: "Bearer" + API_Token,
+                Authorization: API_Token,
                 "Content-Type": "application/json;charset=utf-8"
             },
         }).then((result) => result.json())
